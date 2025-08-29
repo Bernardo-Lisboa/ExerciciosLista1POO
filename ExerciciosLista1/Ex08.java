b49449585dde874b91f21ae37bce4caaffa33587
Bernardo-Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2025-08-29 10:48:05 -0300

    Update Ex08.java

--- ExerciciosLista1/Ex08.java
+++ ExerciciosLista1/Ex08.java
@@ -2,10 +2,11 @@ package ExerciciosLista1;
 
 public class Ex08 {
     public static void main(String[] args) {
-        int matriz[][]= new int[5][5];
+        int matriz[][]= new int[5][5]; //criação da matriz 5x5
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz.length; j++) {
                 if (i==j) {
+                    //quando i=j ele escreve na diagonal principal 1
                     matriz[i][j]=1;
                     System.out.print(matriz[i][j]);
                 }
