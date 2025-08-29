79214323ad5d35c631b5e39d0fd81d83168505a7
Bernardo-Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2025-08-29 10:47:17 -0300

    Update Ex02.java

--- ExerciciosLista1/Ex02.java
+++ ExerciciosLista1/Ex02.java
@@ -11,7 +11,7 @@ public class Ex02 {
         int somaNotas = 0;
         System.out.print("Digite a frequência do aluno: ");
         int freq = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine(); //Buffer para poder ler um int depois
         for (int i = 0; i < notaProva.length; i++) {
             System.out.print("Digite a nota da prova "+(i+1)+": ");
             notaProva[i]= sc.nextInt();
@@ -26,7 +26,9 @@ public class Ex02 {
             somaProj+=notaProjeto[i];
         }
         double mediaProj = somaProj/2;
+        //essa é a equação que tem no exercicio
         double r = Math.round((((3*media)+(2*mediaProj))/5)*Math.floor(media/6)*Math.floor(mediaProj/6));
+        //o Math.round arredonda para o inteiro mais proximo e o Math.floor só corta o decimal por exemplo 6,7 vira 6
         System.out.println("Resultado media = "+r);
         if (freq>74 && r>5) {
             System.out.println("Aluno aprovado");
